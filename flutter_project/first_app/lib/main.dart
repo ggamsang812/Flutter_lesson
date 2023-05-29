ffb536a90570eefff3ba5659f7460d2818584831
ggamsang812 <ggamsang812@gmail.com>
2023-05-29 12:32:12 -0400

    05.29

--- flutter_project/first_app/lib/main.dart
+++ flutter_project/first_app/lib/main.dart
@@ -18,10 +18,10 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('First App'),
         ),
-        body: Center(
+        body: const Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
+            children: [
               Text(
                 'Hello World!',
               ),
