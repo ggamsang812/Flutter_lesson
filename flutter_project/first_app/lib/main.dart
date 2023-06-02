248a539d738643642b3ecd312da1b9bfe8fd1824
ggamsang812 <ggamsang812@gmail.com>
2023-06-02 12:38:12 -0400

    lesson32 + shake dp added

--- flutter_project/first_app/lib/main.dart
+++ flutter_project/first_app/lib/main.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:shake/shake.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 52, 0, 141),
+                Color.fromARGB(255, 231, 202, 248)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
         ),
       ),
     ),
   );
 }
+
+ShakeDetector detector = ShakeDetector.autoStart(onPhoneShake: () {
+  debugPrint('shaking');
+});
