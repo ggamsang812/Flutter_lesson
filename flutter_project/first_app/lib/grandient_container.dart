031f7803466fb40e60773a54d9b7b0158a72432f
ggamsang812 <ggamsang812@gmail.com>
2023-06-07 19:31:38 -0400

    6.7

--- flutter_project/first_app/lib/grandient_container.dart
+++ flutter_project/first_app/lib/grandient_container.dart
@@ -17,7 +17,7 @@ class GradientContainer extends StatelessWidget {
 
   void rollDice() {
     // ...
-    //
+    // need to add function here 
   }
 
   @override
@@ -39,15 +39,16 @@ class GradientContainer extends StatelessWidget {
               width: 200,
             ),
             TextButton(
-                onPressed: rollDice,
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.only(
-                    top: 20,
-                  ),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(fontSize: 28),
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(
+                  top: 20,
                 ),
-                child: const Text('Roll Dice'))
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28),
+              ),
+              child: const Text('Roll Dice'),
+            )
           ],
         ),
       ),
